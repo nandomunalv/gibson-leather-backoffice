5c4e5e7501c1e21054e4f1678a6a6dd3a00cfb61
Fernando <i201511184@cibertec.edu.pe>
2021-02-16 20:58:54 -0500

    Actualización de rutas

--- config/imagemin.js
+++ config/imagemin.js
@@ -1,10 +1,10 @@
 import imagemin from 'imagemin';
-import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminMozJpeg from 'imagemin-mozjpeg';
 
 const input = 'public/img/tmp';
 const output = 'public/img/compress';
 const components = 'public/img/components/products';
 
 export const compress = async () => {
-    return await imagemin([`${input}/*.{jpg,JPG}`], {destination: `${output}`, plugins: [imageminMozjpeg({quality: 50})]});   
+    return await imagemin([`${input}/*.{jpg,JPG}`], {destination: `${output}`, plugins: [imageminMozJpeg({quality: 50})]});
 };
--- config/multer.js
+++ config/multer.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'public/img/products')
+        cb(null, 'public/img/tmp')
     },
     filename: function(req, file, cb) {
         console.log("file", file)
