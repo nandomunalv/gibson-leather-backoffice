b64dc7f8125789f58e0e82841b8a7a69037f64a9
Fernando <i201511184@cibertec.edu.pe>
2021-02-23 20:14:57 -0500

    Servicio que actualiza productos ahora puede enviar imagen
    Queda pendiente la validación para la eliminación de imagen antigua.

--- src/Products/ProductAPI.js
+++ src/Products/ProductAPI.js
@@ -24,12 +24,18 @@ router.post('/products', upload.single('sampleFile'), async (req, res) => {
     }
 });
 
-router.post('/products/:id', async (req, res) => {
+router.post('/products/:id', upload.single('sampleFile'), async (req, res) => {
     const {id} = req.params;
     const payload = req.body;
+
+    payload.productImgName = req.file.filename;
+    payload.productImgUrl = `/img/components/products/${req.file.filename}`;
+
     const result = await productService.updatedProductData(id, payload);
 
     if (result.changedRows) {
+        await imgUtil.transferImage(req.file.filename, req.file.destination);
+
         req.flash('successMessage', result.message);
         res.redirect('/products/list?q=');
     } else {
