185e4a93603430af7f70f7861d90e33da0425c7b
Fernando <i201511184@cibertec.edu.pe>
2021-05-12 20:14:06 -0500

    feat(orders): nuevos estados de compra

---
+++ src/glb-order/order.constants.js
@@ -0,0 +1,12 @@
+const ORDER_STATUS = {
+    NEW_ORDER: 'Nuevo Pedido',
+    AWAITING_PAYMENT: 'Esperando Pago',
+    PAYMENT_RECEIVED: 'Pago Confirmado',
+    PAYMENT_UPDATED: 'Pago Actualizado',
+    COMPLETED: '',
+    REFUNDED_PARTIALLY: 'R',
+    REFUNDED: '',
+    CANCELED: '',
+    FAILED: '',
+    EXPIRED: ''
+}
