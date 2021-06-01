d9624b76fdddbd30e185b321fb733be1adf535b5
Fernando <i201511184@cibertec.edu.pe>
2021-05-31 22:47:17 -0500

    fix: actualización en nombre de controlador

--- src/glb-product/index.js
+++ src/glb-product/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const controller = require('./product.contoller');
+const controller = require('./product.controller');
 
 const {ErrorHandler} = require('./../glb-util/errror');
 
