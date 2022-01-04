51ccc7b67cba7cc53427aeeb0cee612d1dd49e37
Fernando <i201511184@cibertec.edu.pe>
2022-01-04 15:08:17 -0500

    feat: actualización de mapper

--- src/glb-order/order.mapper.js
+++ src/glb-order/order.mapper.js
@@ -5,7 +5,12 @@ module.exports.orderMapper = (type, payload) => {
     const orderMapper = {
         customer_id: payload.customerId,
         order_status_code: payload.status,
-        order_tracking_code: payload.trackingCode,
+        order_code: payload.order,
+        subtotal: payload.subtotal,
+        discount: payload.discount,
+        discount_amount: payload.discountAmount,
+        tax: payload.tax,
+        tax_amount: payload.taxAmount,
         total_price: payload.totalPrice,
         date_order_placed: payload.dateOrderPlaced,
         date_order_delivered: payload.dateOrderDelivered,
